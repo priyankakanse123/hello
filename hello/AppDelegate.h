64b8d29619bb564650b4a8a0117f1866d5a3e273
BridgeLabz <bridgelabz@narayans-macbook-pro-2.local>
2015-12-22 11:30:35 +0530

    Initial Commit

---
+++ hello/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  hello
+//
+//  Created by BridgeLabz on 22/12/15.
+//  Copyright © 2015 BridgeLabz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
